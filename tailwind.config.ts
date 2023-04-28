dfbd428e7414a038c67d05db6dac326b722ea265
to-my-learning-path <115326302+to-my-learning-path@users.noreply.github.com>
2023-04-28 19:44:47 -0400

    Added Tailwind config custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,38 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          main: "var(--color-bg-main)",
+          toggle: "var(--color-bg-toggle)",
+          keypad: "var(--color-bg-keypad)",
+          screen: "var(--color-bg-screen)",
+        },
+        oneKeys: {
+          background: "var()",
+          shadow: "var()",
+        },
+        twoKeys: {
+          background: "var()",
+          shadow: "var()",
+        },
+        threeKeys: {
+          background: "var()",
+          shadow: "var()",
+        },
+        text: {
+          one: "var()",
+          two: "var()",
+        },
+      },
+      fontSize: {
+        numbersFontSize: "32px",
+      },
+      fontFamily: {
+        leagueSpartan: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
