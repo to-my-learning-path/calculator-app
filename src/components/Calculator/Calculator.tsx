ec376bf5a68b91cf5b3cd6043a358a9a38772c6b
to-my-learning-path <115326302+to-my-learning-path@users.noreply.github.com>
2023-04-28 22:11:27 -0400

    Added Calculator styles

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 const keysList = [
   [7, 8, 9, "DEL"],
@@ -8,27 +8,32 @@ const keysList = [
 ];
 
 const Calculator = () => {
+  const [result, setResult] = useState(0);
   return (
-    <div>
-      <div>
-        <p>calc</p>
-        <div>
-          <p>Theme</p>
-          <input type="range" name="" id="" />
+    <div className=" p-6 max-w-lg md:mx-auto">
+      <div className=" flex justify-between items-center">
+        <p className=" text-4xl">calc</p>
+        <div className=" flex justify-between gap-2">
+          <p className=" uppercase tracking-wider">Theme</p>
+          <input type="range" name="" id="" min={1} max={3} step={1} />
         </div>
       </div>
-      <div>
-        <p></p>
+      <div className=" bg-background-screen p-4 rounded-lg text-end my-6">
+        <p className=" text-numbersFontSize">{result}</p>
       </div>
-      <div>
-        <div>
+      <div className=" bg-background-keypad rounded-lg p-6">
+        <div className=" grid grid-cols-4 gap-3">
           {keysList.flat().map((item, index) => (
             <KeyButton key={index} keyValue={item} />
           ))}
         </div>
-        <div>
-          <button>Reset</button>
-          <button>=</button>
+        <div className=" flex justify-between items-center gap-2 pt-4">
+          <button className=" uppercase bg-resetKey-background shadow-keyShadow shadow-resetKey-shadow w-full rounded-md py-3">
+            Reset
+          </button>
+          <button className=" bg-equalKey-background shadow-equalKey-shadow shadow-keyShadow w-full rounded-md py-3">
+            =
+          </button>
         </div>
       </div>
     </div>
@@ -36,7 +41,18 @@ const Calculator = () => {
 };
 
 const KeyButton = ({ keyValue }: { keyValue: string | number }) => {
-  return <button onClick={() => console.log(keyValue)}>{keyValue}</button>;
+  return (
+    <button
+      className={`${
+        keyValue !== "DEL"
+          ? "bg-numberKey-background shadow-numberKey-shadow text-text-numberKey pt-2 hover:bg-opacity-10"
+          : "bg-resetKey-background shadow-resetKey-shadow text-text-primary text-lg pt-1"
+      } text-center rounded-lg text-numbersFontSize shadow-keyShadow`}
+      onClick={() => console.log(keyValue)}
+    >
+      {keyValue}
+    </button>
+  );
 };
 
 export default Calculator;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,33 +3,37 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        background: {
-          main: "var(--color-bg-main)",
-          toggle: "var(--color-bg-toggle)",
-          keypad: "var(--color-bg-keypad)",
-          screen: "var(--color-bg-screen)",
-        },
-        oneKeys: {
-          background: "var()",
-          shadow: "var()",
-        },
-        twoKeys: {
-          background: "var()",
-          shadow: "var()",
-        },
-        threeKeys: {
-          background: "var()",
-          shadow: "var()",
-        },
-        text: {
-          one: "var()",
-          two: "var()",
-        },
+    colors: {
+      background: {
+        main: "var(--color-bg-main)",
+        toggle: "var(--color-bg-toggle)",
+        keypad: "var(--color-bg-keypad)",
+        screen: "var(--color-bg-screen)",
       },
+      resetKey: {
+        background: "var(--color-resetKey-background)",
+        shadow: "var(--color-resetKey-shadow)",
+      },
+      equalKey: {
+        background: "var(--color-equalKey-background)",
+        shadow: "var(--color-equalKey-shadow)",
+      },
+      numberKey: {
+        background: "var(--color-numberKey-background)",
+        shadow: "var(--color-numberKey-shadow)",
+      },
+      text: {
+        numberKey: "var(--color-numberKey-text)",
+        primary: "var(--color-primary-text)",
+      },
+    },
+
+    extend: {
       fontSize: {
         numbersFontSize: "32px",
+      },
+      boxShadow: {
+        keyShadow: "0px 5px 0px 0px",
       },
       fontFamily: {
         leagueSpartan: ["League Spartan", "sans-serif"],
